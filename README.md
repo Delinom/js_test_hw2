491386196618fe4e85b4960a0dd2fdbd9cd1e6b2
Test <test@mail.ru>
2026-03-18 18:51:36 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+![js_test1_hw2 CI](https://github.com/Delinom/js_test_hw2/actions/workflows/ci.yml/badge.svg)
+
---
+++ src/js/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import sortHeroesByHealth from '../index.js';
+
+test('sort heroes by health from max to min', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(sortHeroesByHealth(heroes)).toEqual(expected);
+});
